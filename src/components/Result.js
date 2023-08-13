202b2f0e8726f3b6bb7f62ce8df274a0aa2c5adf
Alexsandr <lurt3t87@mail.ru>
2023-08-13 23:12:23 +0300

    no more changes

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ const Result = ({correct}) => {
     return (
         <div className="Result">
             <div className="result-text">
-                You answer on {correct}/4 questions
+                You answer on {correct}/6 questions
             </div>
             <div className="result-btn"><a href="https://1urt3t.github.io/swimming-quiz/">Again</a></div>
         </div>
